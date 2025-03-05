b5135da66bb559df9a38720137382190693b19de
Willians Gomes Nunes <willians.gnunes01@gmail.com>
2025-03-05 10:27:13 -0400

    usando bootstrap e melhorando estrutura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { UploadS3LogModule } from './modules/upload_s3_log/upload_s3_log.module';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DatabaseModule],
+  imports: [AuthModule, UsersModule, UploadS3LogModule, DatabaseModule],
   controllers: [AppController],
   providers: [],
 })
--- src/common/helpers/hbs-functions.ts
+++ src/common/helpers/hbs-functions.ts
@@ -4,6 +4,28 @@ const helpers = {
     return locale.toLocaleString('pt-BR');
   },
   inc: (value: string) => parseInt(value) + 1,
+  ifCond: function (v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return v1 == v2 ? options.fn(this) : options.inverse(this);
+      case '===':
+        return v1 === v2 ? options.fn(this) : options.inverse(this);
+      case '!=':
+        return v1 != v2 ? options.fn(this) : options.inverse(this);
+      case '!==':
+        return v1 !== v2 ? options.fn(this) : options.inverse(this);
+      case '<':
+        return v1 < v2 ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return v1 <= v2 ? options.fn(this) : options.inverse(this);
+      case '>':
+        return v1 > v2 ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return v1 >= v2 ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
+  },
 };
 
 export const hbsRegisterHelpers = (hbs: any) => {
